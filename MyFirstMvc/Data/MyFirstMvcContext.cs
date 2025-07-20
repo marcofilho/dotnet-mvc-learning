fac16ee7ab242c4a415670fd6d07a52cc118b9ed
Marco Filho <m.barceloslima@gmail.com>
2025-07-20 13:42:20 -0700

    Implement CRUD functionality for Movie management
    Created MovieController for managing movie actions, integrated Entity Framework Core for database interactions, and updated project files for database configuration. Added Razor views for Create, Read, Update, and Delete operations, while removing the old Add.cshtml. Updated navigation and connection settings to support the new structure.

---
+++ MyFirstMvc/Data/MyFirstMvcContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyFirstMvc.Data
+{
+    public class MyFirstMvcContext : DbContext
+    {
+        public MyFirstMvcContext (DbContextOptions<MyFirstMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MyFirstMvc.Models.Movie> Movie { get; set; } = default!;
+    }
+}
--- MyFirstMvc/Program.cs
+++ MyFirstMvc/Program.cs
@@ -1,4 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MyFirstMvc.Data;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<MyFirstMvcContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MyFirstMvcContext") ?? throw new InvalidOperationException("Connection string 'MyFirstMvcContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
